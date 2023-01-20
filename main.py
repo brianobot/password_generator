1ae39cf08fa42e3fdab84e382f548984f93dfadf
Brian Obot <brianobot9@gmail.com>
2023-01-20 17:59:32 +0100

    Add comment documentation to the script

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+"""
+Pure python generator script.
+Generates and returns a specified length of password string.
+if a length is not specified, returns the default password length
+as defined in the global scope of the script module.
+
+Copyright 2023 Brian Obot
+"""
+
 from string import ascii_letters, digits, punctuation
 from random import choice
 
-DEFAULT_PASSWORD_LENGTH = 30
+DEFAULT_PASSWORD_LENGTH = 8
 
 try:
     arg = sys.argv[1]
 except:
     arg = DEFAULT_PASSWORD_LENGTH
 
-def generate_password(arg=None):
+def generate_password(length=None):
     ALL_CHARS = ascii_letters + digits + punctuation
-    password = ""
-    for char in range(arg):
-        password += choice(ALL_CHARS)
-    return password
+    password = []
+    for char in range(length):
+        password.append(choice(ALL_CHARS))
+    return "".join(password)
 
 if __name__ == "__main__":
     password = generate_password(arg)
